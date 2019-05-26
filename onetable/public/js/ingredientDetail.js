bbd227053d89a0d2da444d8859c1ba74990357ce
박현주 <phj2309@naver.com>
2019-05-26 16:51:27 +0900

    팝업창이랑 재료시가뷰
    수정해주떼연~

---
+++ onetable/public/js/ingredientDetail.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+  var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    min: 3,
+                    max: 20
+                }
+            }]
+        }
+    }
+});
+}
