1e5bd22beb077bab8720e78f754c74eb6b755daa
kimbogyu <bg0820@naver.com>
2019-05-31 12:23:45 +0900

    레시피 검색 기능 구현 완료

--- onetable/public/js/recipe.js
+++ onetable/public/js/recipe.js
@@ -8,33 +8,58 @@ $(document).ready(function(){
         for(var i = 0 ; i < result.data.obj.length; i++)
         {
             var data = result.data.obj[i];
-            
-            var priceStr = data.price == null ? '가격 미제공' : addComma(data.price) + "원";
-            var recipeDate = getFormdate(new Date(data.recipeDate), 'min');
-            var listItemHTML = "";
-            listItemHTML += "<li>";
-            listItemHTML += "<div class='listItem'>";
-            listItemHTML += "<div class='listItemHeader'>"; 
-            listItemHTML += "<img src='img/user.svg' width='32px' height='32px'/>";
-            listItemHTML += "<p>" + data.nickName + "</p>";
-            listItemHTML += "</div>";
-            listItemHTML += "<div class='listItemContent'>";
-            listItemHTML += "<div class='imgWrap'>";
-            listItemHTML += "<img src='" + data.recipeImg + "'>";
-            listItemHTML += "</div>";
-            listItemHTML += "<div class='info'>";
-            listItemHTML += "<p class='title'>" + data.name + "</p>";
-            listItemHTML += "<div class='footerInfo'>";
-            listItemHTML += "<p class='price'>" + priceStr + "</p>";
-            listItemHTML += "<p class='createDate'>" +recipeDate + "</p>";
-            listItemHTML += "</div>";
-            listItemHTML += "</div>";
-            listItemHTML += "</div>";
-            listItemHTML += "<div class='wrapper'></div>";
-            listItemHTML += "</div>";
-            listItemHTML += "</li>";
+            var listItemHTML = addItem(data);
 
             $('#listArea').append(listItemHTML);
         }
     });  
 });
+
+function addItem(data) {
+	var priceStr = data.price == null ? '가격 미제공' : addComma(data.price) + "원";
+    var recipeDate = getFormdate(new Date(data.recipeDate), 'min');
+  
+	var listItemHTML = "";
+	listItemHTML += "<li>";
+    listItemHTML += "<div class='listItem'>";
+    listItemHTML += "<div class='listItemHeader'>"; 
+    listItemHTML += "<img src='img/user.svg' width='32px' height='32px'/>";
+    listItemHTML += "<p>" + data.nickName + "</p>";
+    listItemHTML += "</div>";
+    listItemHTML += "<div class='listItemContent'>";
+    listItemHTML += "<div class='imgWrap'>";
+    listItemHTML += "<img src='" + data.recipeImg + "'>";
+    listItemHTML += "</div>";
+    listItemHTML += "<div class='info'>";
+    listItemHTML += "<p class='title'>" + data.name + "</p>";
+    listItemHTML += "<div class='footerInfo'>";
+    listItemHTML += "<p class='price'>" + priceStr + "</p>";
+    listItemHTML += "<p class='createDate'>" +recipeDate + "</p>";
+    listItemHTML += "</div>";
+    listItemHTML += "</div>";
+    listItemHTML += "</div>";
+    listItemHTML += "<div class='wrapper'></div>";
+    listItemHTML += "</div>";
+    listItemHTML += "</li>";
+  
+	return listItemHTML;
+  }
+
+
+$('#recipeSearhBtn').click(function() {
+	$('#listArea').empty();
+
+	serverRequest("http://1.240.181.56:8080/recipe/search", "GET", {
+	  page: 1,
+	  itemNum: 30,
+	  query: $('#recipeSearhValue').val()
+	}, 'json').then(function(result) {
+	  for(var i = 0 ; i < result.data.obj.length; i++)
+	  {
+		let data = result.data.obj[i];
+		let listItemHTML  = addItem(data);
+	   
+		$('#listArea').append(listItemHTML);
+	  }  
+	});
+  });
