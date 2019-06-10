8cfb4f287d4b1f812bbbdbb02239f63292ca856c
박현주 <phj2309@naver.com>
2019-06-10 18:54:56 +0900

    Auto stash before merge of "master" and "origin/master"

---
+++ onetable/public/js/ranking.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  serverRequest("http://1.240.181.56:8080/ingredient/search/all", "GET", {
+        page: 1,
+        itemNum: 5
+      }, 'json').then(function(result) {
+        for(var i = 0 ; i < result.data.length; i++)
+        {
+          var data = result.data[i];
+
+          var priceStr = addComma(data.price) + "원";
+          var recipeDate = getFormdate(new Date(data.priceDate), 'min');
+
+          var listItemHTML = "";
+
+          listItemHTML += "<li>";
+          listItemHTML += "<div class='listItem'>";
+          listItemHTML += "<div class='listItemContent'>";
+          listItemHTML += "<div class='imgWrap'>";
+          listItemHTML += "<img src='" + data.imgUrl + "'>";
+          listItemHTML += "</div>";
+          listItemHTML += "<div class='info'>";
+          listItemHTML += "<p class='title'>" + data.displayName + "</p>";
+          listItemHTML += "<div class='footerInfo'>";
+          listItemHTML += "<p class='price'>" + priceStr + "</p>";
+          listItemHTML += "<p class='createDate'>" +recipeDate + "</p>";
+          listItemHTML += "</div>";
+          listItemHTML += "</div>";
+          listItemHTML += "</div>";
+          listItemHTML += "<div class='wrapper' jControll='ingredientDetailView' jData='" + data.ingredientItemId +"'></div>";
+          listItemHTML += "</div>";
+          listItemHTML += "</li>";
+
+          $('#listArea').append(listItemHTML);
+        }
+      });
+})
